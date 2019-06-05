3b8703d4f6b5661cb46c63ec1da9a3c74d5ee7b3
gilberto <gilberto.vampre@academico.ifg.edu.br>
2019-06-05 18:26:19 -0300

    completando a ontologia e consulta estruturada

--- sparql_query.py
+++ sparql_query.py
@@ -1,49 +1,50 @@
 from owlready2 import *
-from pprint import pprint
-
+import json
+import pprint
 
 class SparqlQuery:
+    vms = ["alibaba", "aws", "azure", "google"]
 
     def __init__(self):
+
         onto_path.append("/home/gilberto/PycharmProjects/cloud_ontology/Ontology/")
 
-        default_world.get_ontology("CloudOntology.owl").load()
+        default_world.get_ontology("CloudIaaS3.owl").load()
 
         sync_reasoner()  # reasoner is started and synchronized here
-
         self.graph = default_world.as_rdflib_graph()
 
     def search(self):
-        # Search query is given here
-        # Base URL of your ontology has to be given here
-        query = """ PREFIX ab: <http://www.owl-ontologies.com/CloudOntology.owl>
-                SELECT ?s ?p ?o
-                WHERE {
-                ?s ?p ?o .
-                } """
-        print(query)
-        # query is being run
-        resultsList = self.graph.query(query)
 
-        # creating json object
-        response = []
-        for item in resultsList:
+        response = {}
+        iaasProvider = {}
 
-            s = str(item['s'].toPython())
-            s = re.sub(r'.*#', "", s)
+        response['generic_fields'] = ['cpu', 'hd', 'pricing', 'ram']
 
-            p = str(item['p'].toPython())
-            p = re.sub(r'.*#', "", p)
+        for provider in SparqlQuery.vms:
 
-            o = str(item['o'].toPython())
-            o = re.sub(r'.*#', "", o)
+            iaasProvider[provider] = []
 
-            response.append({'s': s, 'p': p, 'o': o})
+            query = """
+                            PREFIX b:<http://www.semanticweb.org/gilberto/ontologies/2019/4/CloudIaaS#>
+                            SELECT DISTINCT ?o
+                            WHERE  {{
+                            b:{} ?p ?s .
+                            ?s rdfs:comment ?o .
+                            }} """.format(provider)
 
+            resultsList = self.graph.query(query)
+
+            for item in resultsList:
+
+                o = str(item['o'].toPython())
+                o = re.sub(r'.*#', "", o)
+
+                iaasProvider[provider].append(o)
+
+        response['specific_fields'] = iaasProvider
         return response
-
 
 runQuery = SparqlQuery()
 response = runQuery.search()
-
-pprint(response)  # just to show the output
+pprint.pprint(response)  # just to show the output
